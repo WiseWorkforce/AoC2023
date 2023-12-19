49eda12a9e26a2b3dd02c8d7faa5dca1d8631bec
WiseWorkforce <153016729+WiseWorkforce@users.noreply.github.com>
2023-12-19 23:22:24 +0100

    Day 4 - Part 1

---
+++ Day 4/Part 1/main.py
@@ -0,0 +1,23 @@
+file_to_open = r'Day 4\Part 1\input.txt'
+grid = open(file_to_open).read().splitlines()
+
+prev_card = 0
+sum_value = 0
+for row in grid:
+    card_number = row.split(':')[0]
+    rest_of_card = row.split(':')[1]
+    winning_str = rest_of_card.split('|')[0].strip().split(' ')
+    winning_str = [x for x in winning_str if x != '']
+    have_str = rest_of_card.split('|')[1].strip().split(' ')
+    have_str = [x for x in have_str if x != '']
+    multiplier = 0
+    counter = 0
+    for i in have_str:
+        if i in winning_str:
+            counter += 1
+            if multiplier == 0 :
+                multiplier = 1
+            else:
+                multiplier = multiplier * 2
+    sum_value += multiplier
+print(sum_value)
